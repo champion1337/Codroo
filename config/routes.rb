83534bb2f46a15ca82852789fad1ddbc3d52c99e
Champion1337 <champion1337@gmail.com>
2010-11-01 23:45:15 +0200

    Users model, controller, views and authlogin system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 CodrooRor::Application.routes.draw do
+  get "user_sessions/new"
+
   get "home/index"
 
+  resources :user_sessions
+  match 'login' => "user_sessions#new", :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
+  match 'account' => "user#my_account", :as => :account
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
