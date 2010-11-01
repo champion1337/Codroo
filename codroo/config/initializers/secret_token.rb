dca80e3837f5592419a764f8dee2fff82a044c6b
Champion1337 <champion1337@gmail.com>
2010-11-01 13:43:15 +0200

    initiated rails

---
+++ codroo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codroo::Application.config.secret_token = '8ecaea591f5a74e5a6e3a6fc93a6be1687167166a58cc68192b5d08c0ec3da487214b86734aac2a0e1556ae0d44212882f64c3fa20bbe065b4c2f7419f29739e'
